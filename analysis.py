03ada654dc1d22f3a4d85aafa333042dbf052819
ImportMengjie <limengjie@hotmail.com>
2021-09-30 16:22:08 +0800

    add analysis path count hypothesis testing

--- analysis.py
+++ analysis.py
@@ -1,11 +1,12 @@
 import os
 import json
 
+import scipy.stats
 import torch
 
 from utils import config
 from utils import draw_line
-from utils import DenoisingMethodLS
+from utils import DenoisingMethodLS, DenoisingMethodMMSE
 from loader import CsiDataloader, DataType
 
 import numpy as np
@@ -24,10 +25,18 @@ def analysis_loss_nmse():
                       save_dir=config.RESULT_IMG, diff_line_markers=True)
 
 
-def analysis_dft_denosing(data_path, fix_snr, max_count, path_start, path_end=None):
+def flatten_complex(a):
+    a = a.reshape(a.shape + (1,))
+    a = np.concatenate((np.real(a), np.imag(a)), axis=-1).flatten()
+    return a
+
+
+def analysis_dft_denosing(data_path, fix_snr, max_count, path_start, path_end=None, cp=None):
     csi_loader = CsiDataloader(data_path, train_data_radio=1)
     if path_end is None:
         path_end = csi_loader.n_sc + 1
+    if cp is None:
+        cp = path_end
     xp = csi_loader.get_pilot_x()
     h = csi_loader.get_h(DataType.train)
     hx = h @ xp
@@ -41,29 +50,97 @@ def analysis_dft_denosing(data_path, fix_snr, max_count, path_start, path_end=No
     g = g[0:max_count]
 
     g_hat_idft = np.fft.ifft(g_hat, axis=-2)
+    g_idft = np.fft.ifft(g, axis=-2)
     nmse_dict = {str(i): [] for i in range(g.shape[0])}
-    nmse_dict.update({str(i)+'ls': [] for i in range(g.shape[0])})
-    for path_count in range(path_start, path_end):
+    nmse_dict.update({str(i) + 'ls': [] for i in range(g.shape[0])})
+    true_nmse_dict = {str(i): [] for i in range(g.shape[0])}
+    noise_var = {str(i): [] for i in range(g.shape[0])}
+    noise_mean = {str(i): [] for i in range(g.shape[0])}
+    noise_up = {str(i): [] for i in range(g.shape[0])}
+    chuck_noise = {str(i): [] for i in range(g.shape[0])}
+    p_value = {str(i): [] for i in range(g.shape[0])}
+
+    chuck_noise_fix = {str(i) + '-chuck_noise_var_fix': [] for i in range(g.shape[0])}
+
+    def get_path_data(path_count):
         chuck_array = np.concatenate((np.ones(path_count), np.zeros(csi_loader.n_sc - path_count))).reshape((-1, 1))
-        not_chuck_array = np.concatenate((np.zeros(csi_loader.n_sc - path_count), np.ones(path_count))).reshape((-1, 1))
+        not_chuck_array = np.concatenate(
+            (np.full((path_count,), False), np.full((csi_loader.n_sc - path_count,), True)))
         g_hat_idft_chuck = g_hat_idft * chuck_array
-        g_hat_idft_not_chuck = g_hat_idft * chuck_array
+        g_idft_chuck = g_idft * chuck_array
+        g_hat_idft_noise = g_hat_idft[:, not_chuck_array, :]
         g_hat_chuck = np.fft.fft(g_hat_idft_chuck, axis=-2)
+        g_chuck = np.fft.fft(g_idft_chuck, axis=-2)
+        return chuck_array, not_chuck_array, g_hat_idft_chuck, g_idft_chuck, g_hat_idft_noise, g_hat_chuck, g_chuck
+
+    _, _, _, _, cp_hat_idft_noise, _, _ = get_path_data(cp)
+
+    for path_count in range(path_start, path_end):
+        chuck_array, not_chuck_array, g_hat_idft_chuck, g_idft_chuck, g_hat_idft_noise, g_hat_chuck, g_chuck = get_path_data(
+            path_count)
         for i in range(g_hat_chuck.shape[0]):
             i_g_hat_chuck = g_hat_chuck[i]
             i_g = g[i]
+            i_g_chuck = g_chuck[i]
             i_g_hat = g_hat[i]
-            i_nmse = 10*np.log10(((np.abs(i_g-i_g_hat_chuck)**2).sum()/(np.abs(i_g)**2).sum()).mean())
+            i_g_idft_noise = g_hat_idft_noise[i]
+            i_nmse = 10 * np.log10(((np.abs(i_g - i_g_hat_chuck) ** 2).sum() / (np.abs(i_g) ** 2).sum()).mean())
             nmse_dict[str(i)].append(i_nmse)
-            ls_nmse = 10*np.log10(((np.abs(i_g-i_g_hat)**2).sum()/(np.abs(i_g)**2).sum()).mean())
-            nmse_dict[str(i)+'ls'].append(ls_nmse)
+            ls_nmse = 10 * np.log10(((np.abs(i_g - i_g_hat) ** 2).sum() / (np.abs(i_g) ** 2).sum()).mean())
+            nmse_dict[str(i) + 'ls'].append(ls_nmse)
 
+            i_true_nmse = 10 * np.log10(((np.abs(i_g - i_g_chuck) ** 2).sum() / (np.abs(i_g) ** 2).sum()).mean())
+            true_nmse_dict[str(i)].append(i_true_nmse)
+
+            i_n = (i_g_hat_chuck - i_g_hat)
+            i_n = i_n.reshape(i_n.shape + (1,))
+            i_n = np.concatenate((np.real(i_n), np.imag(i_n)), axis=-1).flatten()
+            # noise_mean[str(i)].append(i_n.mean())
+            noise_var[str(i)].append(i_n.var())
+            # noise_up[str(i)].append((i_n > i_n.mean()).sum() / len(i_n))
+
+            i_g_idft_noise = i_g_idft_noise.reshape(i_g_idft_noise.shape + (1,))
+            i_g_idft_noise = np.concatenate((np.real(i_g_idft_noise), np.imag(i_g_idft_noise)), axis=-1).flatten()
+            chuck_noise[str(i)].append(i_g_idft_noise.var())
+            noise_mean[str(i)].append(i_g_idft_noise.mean())
+            noise_up[str(i)].append((i_g_idft_noise > i_g_idft_noise.mean()).sum() / len(i_n))
+            chuck_noise_fix[str(i) + '-chuck_noise_var_fix'].append(
+                i_g_idft_noise.var() * (csi_loader.n_sc - path_count))
+
+            i_cp_hat_idft_noise = cp_hat_idft_noise[i].reshape(cp_hat_idft_noise[i].shape + (1,))
+            i_cp_hat_idft_noise = np.concatenate((np.real(i_cp_hat_idft_noise), np.imag(i_cp_hat_idft_noise)),
+                                                 axis=-1).flatten()
+            i_cp_var = i_cp_hat_idft_noise.var()
+            cur_hat_idft_noise = flatten_complex(g_hat_idft[i, path_count - 1])
+            cur_path_chi = (csi_loader.n_r - 1) * np.var(cur_hat_idft_noise) / i_cp_var
+            cur_path_p = 1 - scipy.stats.chi2(csi_loader.n_r - 1).cdf(cur_path_chi)
+            p_value[str(i)].append(cur_path_p)
+
+    chuck_noise_fix.update(noise_var)
     x = list(range(path_start, path_end))
+    noise_var['var'] = [var.mean().item() / 2 for _ in x]
+    # chuck_noise['var'] = [var.mean().item() / 2 for _ in x]
+    min_path_txt = 'min_path'
+    for k, v in nmse_dict.items():
+        if k.isdigit():
+            min_path = np.array(v).argmin() + path_start
+            min_path_txt += '{}:{};'.format(k, min_path)
 
-    draw_line(x, nmse_dict, xlabel='chuck_path', diff_line_markers=True)
+    title = '{}'.format(csi_loader)
+    draw_line(x, nmse_dict, title="{}-{}db".format(title,fix_snr) + min_path_txt, xlabel='chuck_path', diff_line_markers=False)
+    # draw_line(x, true_nmse_dict, title="true{}db".format(fix_snr) + min_path_txt, xlabel='chuck_path',
+    #           diff_line_markers=True)
+    # draw_line(x, noise_mean, xlabel='chuck_path', ylabel='mean', diff_line_markers=True)
+    draw_line(x, noise_var, title=title, xlabel='chuck_path', ylabel='var', diff_line_markers=True)
+    # draw_line(x, noise_up, xlabel='chuck_path', ylabel='up/total', diff_line_markers=True)
+    draw_line(x, chuck_noise, xlabel='chuck_path', ylabel='var', title=title + '-chuck-var', diff_line_markers=True)
+    draw_line(x, chuck_noise_fix, xlabel='chuck_path', ylabel='var', title='compare', diff_line_markers=True)
+    draw_line(x, p_value, xlabel='chuck_path', ylabel='p-value', title='{}-p_value'.format(title), diff_line_markers=True)
 
 
 if __name__ == '__main__':
     # analysis_loss_nmse()
+    # analysis_dft_denosing(data_path="data/spatial_mu_ULA_32_16_64_10_l10_11.mat", fix_snr=10, max_count=3, path_start=1,
+    #                       path_end=20)
     analysis_dft_denosing(data_path="data/3gpp_mu_32_16_512_5_5.mat", fix_snr=35, max_count=3, path_start=1,
-                          path_end=50)
+                          path_end=30)
